e208e0739a2164f39abd8b53ad63b752c9ef6b91
Morgan Murrah <mmurrah@airbridge.ac.nz>
2017-10-14 19:58:13 -0400

    Templated Poster in Vue

---
+++ bauhaus-vue-component.js
@@ -0,0 +1,17 @@
+var layout = new Vue({
+    el: "#layout",
+    data: {
+        who: "Who",
+        description: "A event",
+        title: "Title",
+        what: "What",
+        number: "123",
+        word: "Location",
+        address1: "Room #",
+        address2: "Name of building",
+        address3: "Address cont.",
+        contact1: "Contact",
+        contact2: "Email",
+        contact3: "Website"
+    }
+  });
